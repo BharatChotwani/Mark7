667415bb54394645b2bad88240053b204a6350bb
Bharat <bchotwani3@gmail.com>
2021-06-08 19:31:17 +0530

    added event

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ var txtInput = document.querySelector("#input-txt");
 var btnTranslator = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#output-txt");
 
+function clickHandler() {
+    var inputText = txtInput.value;
+    inputText = txtOutput.innerText
+};
+
+btnTranslator.addEventListener("click", clickHandler)
