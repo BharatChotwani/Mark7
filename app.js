d42e8ea7a83ee60ee7e3411dddaf81ce99708e8b
Bharat <bchotwani3@gmail.com>
2021-06-08 19:34:43 +0530

    import API

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var txtInput = document.querySelector("#input-txt");
 var btnTranslator = document.querySelector("#btn-translate");
 var txtOutput = document.querySelector("#output-txt");
 
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+};
+
 function clickHandler() {
     var inputText = txtInput.value;
     inputText = txtOutput.innerText
