cd55428714d7adbcdcdc1944286237b1db854315
Bharat <bchotwani3@gmail.com>
2021-06-09 14:04:37 +0530

    added error handler

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ function getTranslationURL(text){
     return serverURL + "?" + "text=" + text
 };
 
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("max limit reached")
+};
+
 function clickHandler() {
     var inputText = txtInput.value;
     
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
     .then(data => {
-        var textTranslate = console.log(data);
+        var textTranslate = data.contents.translated;
         txtOutput.innerText = textTranslate;
-    });
-
+    })
+    .catch (errorHandler)
 };
 
 btnTranslator.addEventListener("click", clickHandler)
